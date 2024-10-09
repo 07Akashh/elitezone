fdffceb37186d7666e11743123f117b4892fbff5
07Akashh <aonecreations1@gmail.com>
2024-10-09 22:53:41 +0530

    serverurl

--- src/config/serverUrl.js
+++ src/config/serverUrl.js
@@ -1,5 +1,5 @@
 // const serverUrl = "https://aa5f-2409-40e3-1033-7a2d-a549-f714-e2cf-907e.ngrok-free.app"
-// const serverUrl = "https://ecomm-api-production.up.railway.app"
-const serverUrl = "http://localhost:3002"
+const serverUrl = "https://ecomm-api-production.up.railway.app"
+// const serverUrl = "http://localhost:3002"
 
 export default serverUrl;
